d280aa80efeb73f6950727db079e949767c9d821
madhanse29 <91239353+madhanse29@users.noreply.github.com>
2021-10-13 11:11:53 +0530

    Add files via upload

---
+++ 21task/layout.js
@@ -0,0 +1,36 @@
+
+
+setTimeout(()=>{ document.querySelector(".timer").innerText= "10";
+setTimeout(()=> {
+document.querySelector(".timer").innerText= "9"
+   setTimeout(()=> {
+document.querySelector(".timer").innerText= "8" 
+         setTimeout(()=> {
+document.querySelector(".timer").innerText= "7"
+               setTimeout(()=> {
+document.querySelector(".timer").innerText= "6"
+                     setTimeout(()=> {
+document.querySelector(".timer").innerText= "5" 
+                           setTimeout(()=> {
+document.querySelector(".timer").innerText= "4" 
+                                 setTimeout(()=> {
+document.querySelector(".timer").innerText= "3" 
+                                       setTimeout(()=> {
+document.querySelector(".timer").innerText= "2" 
+                                             setTimeout(()=> {
+document.querySelector(".timer").innerText= "1" 
+                                                   setTimeout(()=> {
+document.querySelector(".timer").innerText= "Happy Independece Day!.." 
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1)
+},1000*1);
+
+
